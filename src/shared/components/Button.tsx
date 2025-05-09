6a8d06ddc1b206b1839880eefd52b226f9fe2399
Takib-Ahmed <satakib300@gmail.com>
2025-05-09 15:35:10 +0600

    color foreground on gmail browser changed to white

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -17,7 +17,9 @@ const Button: React.FC<ButtonProps> = ({
       className={` Gradient-Button  flex items-center justify-center p-2.5 px-5 max-[400px]:py-2 max-[400px]:px-4  min-[375px]:p-3.5 min-[375px]:px-7   sm:py-[20px] sm:px-[32px] gap-[8px]    
 text-sm min-[375px]:text-[15px]  sm:text-[20px]  sm:h-[64px] text-nowrap  leading-[120%] hover: ${className}  `}
     >
-      {children}
+     <span className=" text-white">
+       {children}
+     </span>
     </Link>
   );
 };
--- src/shared/components/IntroCard.tsx
+++ src/shared/components/IntroCard.tsx
@@ -13,10 +13,10 @@ export default function IntroCard({ Data }: IntroCardProps) {
   <div className=" w-full   md:w-[812.0509033203125px]  h-[280px] md:h-[250px]   lg:h-[303.9018859863281px] relative text-center flex  justify-center items-center ">
         <Recentbg className="   inset-0 absolute  -z-10   w-full    md:w-auto" />
         <div className=" flex flex-col  justify-center items-center sm:mt-5 md:mt-10 ">
-          <h1 className="  max-[400px]:text-[25px] text-[30px]  min-[590px]:text-[35px] sm:text-[40px] md:text-[45px]  lg:text-[48px]  font-[700]  ">
+          <h1 className="  text-white max-[400px]:text-[25px] text-[30px]  min-[590px]:text-[35px] sm:text-[40px] md:text-[45px]  lg:text-[48px]  font-[700]  ">
             {Data.title}
           </h1>
-          <p className=" max-[400px]:text-[10px] text-[12px] md:text-[15px]  lg:text-[20px] font-[400]   lg:w-[372px]  ">
+          <p className=" text-white max-[400px]:text-[10px] text-[12px] md:text-[15px]  lg:text-[20px] font-[400]   lg:w-[372px]  ">
             {Data.subtitle}
           </p>
         </div>
