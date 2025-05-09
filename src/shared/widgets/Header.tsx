e36aa65cbd74588cabe196000b3ee41f886ecdaa
Takib-Ahmed <satakib300@gmail.com>
2025-05-09 16:12:06 +0600

    nav menu basic interaction added

--- src/shared/widgets/Header.tsx
+++ src/shared/widgets/Header.tsx
@@ -1,7 +1,9 @@
+"use client"
 import Logo from "@/assets/logo.svg";
 import Menuicn from "@/assets/icons/Menuicn";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 export default function Header() {
   const Navdetails = {
     Logo,
@@ -25,12 +27,15 @@ export default function Header() {
     ],
     Menuicn,
   };
+  const [Isopen,setIsopen] =  useState(true)
   return (
-    <div className=" flex w-full justify-center    ">
+    <div className=" flex  w-full justify-center    ">
       <div
-        className=" bg-translucent-black z-50  backdrop-blur-[30px] flex gap-[8px] justify-between items-center  p-[16px] sm:p-[24px] 
+        className={` bg-translucent-black z-50   flex gap-[8px]  p-[16px] sm:p-[24px] 
        w-[90%]
-      min-[1200px]:w-[1140px]  h-[6%] min-[375px]:h-[60px] min-[555px]:h-[70px] sm:h-[90px] font-DMSans leading-[150%] border-[1px] border-translucent-white  rounded-[80px] fixed  top-[32px]"
+      min-[1200px]:w-[1140px]   transition-all duration-300 ease-in overflow-hidden  h-[6%]  font-DMSans leading-[150%] border-[1px] border-translucent-white   fixed  top-[32px]
+       ${Isopen ?' px-5 rounded-[50px]  backdrop-blur-[30px]  h-fit  flex-col  items-start justify-start ':" rounded-[80px] h-[6%] min-[375px]:h-[60px] min-[555px]:h-[70px] sm:h-[100px] backdrop-blur-[30px] items-center justify-between  "}
+       `}
       >
         <Link href="/">
           <Image
@@ -38,33 +43,31 @@ export default function Header() {
             height={10000}
             src={Navdetails.Logo}
             alt=""
-            className=" z-50  w-5 min-[375px]:w-8 sm:w-10 md:w-[38.49px]  lg:h-[36px]  left-[-1px]  top-[2px]"
+            className={`  z-50  w-5 min-[375px]:w-8 sm:w-10 md:w-[38.49px]  lg:h-[36px]  left-[-1px]  top-[2px] ${
+              Isopen&&  '  mt-5 '
+            }`}
           />
         </Link>
-        <ul className="flex items-center   h-[32px]  justify-between  w-[360px]">
+     
+
+         <ul className={`${Isopen ? "grid  items-start       py-10    w-full  justify-start gap-[8vw]  sm:gap-10   ":'flex items-center   h-[32px]    justify-between  w-[360px]'}`}>
           {Navdetails.navlinks.map((link) => (
             <li key={link.id}>
-              <Link href="#" className=" text-[16px] hidden sm:block  ">
+              
+              <Link href="#" className={Isopen?"text-xs min-[350px]:text-[12px] lg:text-[16px]  sm:block  ":"text-[16px] hidden sm:block  "}>
                 {link.name}
               </Link>
             </li>
           ))}
-          <li className=" sm:hidden  ">
+           <li onClick={()=>{ setIsopen((prev)=>!prev)}} className={` sm:hidden ${Isopen && 'absolute right-5 top-8'}   `}>
             <Link href="#" className=" text-[16px]  ">
               <Navdetails.Menuicn className=" w-[24px] min-[375px]:w-[28px] sm:w-[32px]" />
             </Link>
           </li>
         </ul>
-        <ul className=" flex-col  items-start  hidden      w-full  justify-start  gap-10    ">
-          {Navdetails.navlinks.map((link) => (
-            <li key={link.id}>
-              
-              <Link href="#" className=" text-[16px]  sm:block  ">
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        
+ 
+    
       </div>
     </div>
   );
