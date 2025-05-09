5bdd262d2ac89bd7e4360e8cd2d38658d2b4081f
Takib Ahmed <satakib300@gmail.com>
2025-05-09 16:29:31 +0600

    Update Header.tsx

--- src/shared/widgets/Header.tsx
+++ src/shared/widgets/Header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
     ],
     Menuicn,
   };
-  const [Isopen,setIsopen] =  useState(true)
+  const [Isopen,setIsopen] =  useState(false)
   return (
     <div className=" flex  w-full justify-center    ">
       <div
